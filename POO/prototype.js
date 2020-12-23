68eb2841175ab2f2ce5c137cf9484e967802076b
GustavoGomesDias <treeelevengg@gmail.com>
2020-12-22 23:29:49 -0300

    Prototype

---
+++ POO/prototype.js
@@ -0,0 +1,76 @@
+// 1 - Gasolina | 2 - Diesel | 3 - Álcool
+// 1 - Passeio | 2 - Transporte | 3 - Combate
+
+class Carro{
+    constructor(tpComb, tpTipo){
+        this.tpTipo = tpTipo;
+        this.qtdComb = 100;
+        this.ligado = false;
+        this.tipoComb = tpComb
+    }
+    info(){
+        console.log(this.qtdComb);
+        console.log(this.tipoComb);
+        console.log(this.ligado? 
+            "Sim":"Não"
+        );
+        if(this.tpTipo == 1){
+            console.log("Passeio");
+        }else if(this.tpTipo == 2){
+            console.log("Transporte")
+        }else{
+            console.log("Combate");
+        }
+    }
+    set setLigado(t){
+        this.ligado = t;
+    }
+    get getLigado(){
+        return this.ligado;
+    }
+    set setComb(v){
+        this.comb = v;
+    }
+    get getComb(){
+        return this.comb;
+    }
+}
+
+class CarroCombate extends Carro{
+    constructor(potTiros){
+        // Passa as propriedades de Carro para CarroCombate
+        super(3, 2);
+
+        this.tiros = 1000;
+        this.potTiros = potTiros;
+        this.blindagem = 100;
+    }
+    info(){
+        // Pega o método info() de Carro e permite extende-lo para novas funcionalidades
+        super.info();
+
+        console.log(this.tiros);
+        console.log(this.potTiros);
+        console.log(this.blindagem);
+    }
+}
+
+let c1 = new CarroCombate(100);
+
+
+// Object.prototype => Object se refere a class, Object.prototype aos métodos e propriedades
+
+// Propriedades
+Carro.prototype.potencia = 0;
+Carro.prototype.velocidade = 0;
+Carro.prototype.veloMax = 0;
+
+// Métodos
+Carro.prototype.info2 = function(){
+    this.info();
+    console.log(this.potencia);
+    console.log(this.velocidade);
+    console.log(this.veloMax);
+}
+
+c1.info2();
--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 <h2>E aí...</h2>
 <h1>Esse repositório é sobre meus estudos em JavaScript</h1>
+<h3>Navegação</h3>
+<ol>
+    <li>
+        <a href="https://github.com/GustavoGomesDias/estudos-js#o-que-tem-aqui">O que tem aqui</a>
+    </li>
+</ol>
+
 <h3>O que tem aqui</h3>
 <ol>
     <li>
@@ -18,4 +25,10 @@
         <a href="https://github.com/GustavoGomesDias/estudos-js/tree/master/funcionalidades">Funcionalidades</a>
     </li>
 </ol>
-    
+
+<h3>Minhas referências</h3>
+<ol>
+    <li>
+        <a href="https://www.youtube.com/playlist?list=PLx4x_zx8csUj3IbPQ4_X5jis_SkCol3eC">Playlist do canal CFBCursos</a>
+    </li>
+</ol>

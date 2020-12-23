2e5036f5dc1bac84b420c929e8191978b69a1c0c
GustavoGomesDias <treeelevengg@gmail.com>
2020-12-22 22:07:17 -0300

    POO - Herança

--- POO/poo-heranca.js
+++ POO/poo-heranca.js
@@ -1,5 +1,5 @@
-// 1 - Gasolina 2 - Diesel 3 - Álcool
-// 1 Passeio 2 - Transporte 3 - Combate
+// 1 - Gasolina | 2 - Diesel | 3 - Álcool
+// 1 - Passeio | 2 - Transporte | 3 - Combate
 
 class Carro{
     constructor(tpComb, tpTipo){
@@ -9,12 +9,18 @@ class Carro{
         this.tipoComb = tpComb
     }
     info(){
-        console.log(this.tpTipo);
         console.log(this.qtdComb);
-        console.log(this.tpComb);
+        console.log(this.tipoComb);
         console.log(this.ligado? 
             "Sim":"Não"
         );
+        if(this.tpTipo == 1){
+            console.log("Passeio");
+        }else if(this.tpTipo == 2){
+            console.log("Transporte")
+        }else{
+            console.log("Combate");
+        }
     }
     set setLigado(t){
         this.ligado = t;
@@ -29,3 +35,25 @@ class Carro{
         return this.comb;
     }
 }
+
+class CarroCombate extends Carro{
+    constructor(potTiros){
+        // Passa as propriedades de Carro para CarroCombate
+        super(3, 2);
+
+        this.tiros = 1000;
+        this.potTiros = potTiros;
+        this.blindagem = 100;
+    }
+    info(){
+        // Pega o método info() de Carro e permite extende-lo para novas funcionalidades
+        super.info();
+
+        console.log(this.tiros)
+        console.log(this.potTiros)
+        console.log(this.blindagem)
+    }
+}
+
+let c1 = new CarroCombate(100);
+c1.info();

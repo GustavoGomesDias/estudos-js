8e9cfa727d03b3562575d412d75712fca0537a0b
GustavoGomesDias <treeelevengg@gmail.com>
2021-01-14 23:18:28 -0300

    DELETE E UPDATE

--- node-tuto/ew-cli/database.js
+++ node-tuto/ew-cli/database.js
@@ -1,10 +1,12 @@
 const {
-  readFile
+  readFile,
+  writeFile
 } = require('fs');
 
 const { promisify } = require('util');
 
 const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 // Outra forma de obter dados do json
 // const dadosJson = require('./herois.json');
@@ -20,17 +22,75 @@ class Database {
     return JSON.parse(arquivo.toString());
   }
 
-  escreverArq(){
-    
+  async escreverArq(dados){
+    await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados));
+    return true;
   }
   
+  async cadastrar(heroi){
+    const dados = await this.obterDadosArq();
+    const id = heroi.id <= 2 ? heroi.id : Date.now();
+    
+    // Vai ter um objeto com nome e poder e outro só com id
+    // Devemos juntar ambos
+
+    const heroiComId = {
+      id,
+      ...heroi
+    };
+
+    const dadosFinal = [
+      ...dados,
+      heroiComId
+    ]
+    const resultado = await this.escreverArq(dadosFinal);
+    return resultado;
+  }
+
   async listar(id){
     const dados = await this.obterDadosArq();
 
     const dadosFiltrados = dados.filter(item => (id ? (item.id === id) : true)); // Se n houver id passado, trás a lista completa
-    console.log(dadosFiltrados)
     return dadosFiltrados;
   }
+
+  async remover(id){
+    if(!id){
+      return await this.escreverArq([]);
+    }
+    
+    const dados = await this.obterDadosArq();
+    const indice = dados.findIndex(item => item.id === parseInt(id));
+
+    if(indice === -1){
+      throw Error("Herói inexistente.");
+    }
+    
+    dados.splice(indice, 1);
+    return await this.escreverArq(dados);
+  }
+
+  async atualizar(id, modifi){
+    const dados = await this.obterDadosArq();
+    const indice = dados.findIndex(item => item.id === parseInt(id));
+
+    if(indice === -1){
+      throw Error("Herói inexistente");
+    }
+
+    const atual = dados[indice];
+    const objAtualizar = {
+      ...atual,
+      ...modifi
+    }
+    dados.splice(indice, 1);
+
+    return await this.escreverArq([
+      ...dados,
+      objAtualizar
+    ])
+  }
+
 }
 
 
--- node-tuto/ew-cli/test.js
+++ node-tuto/ew-cli/test.js
@@ -11,23 +11,70 @@ const DEFAULT_ITEM_CADASTRAR = {
   id: 1
 }
 
+const DEFAULT_ITEM_ATUALIZAR = {
+  nome: "Lanterna Verde",
+  poder: "Energia do Anel",
+  id: 2
+}
+
 // descrine => inicializa um suite de testes
 describe('Suite de manipulação de heróis', function(){
+  // Cadastrar os defaults itens
+  before(async () => {
+    await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    await database.cadastrar(DEFAULT_ITEM_ATUALIZAR);
+  })
+
   it('Deve pesquisar um herói usando arquivos', async () => {
     const expected = DEFAULT_ITEM_CADASTRAR;
     const [resultado] = await database.listar(expected.id);
-    
-    console.log('resultado', resultado);
-    console.log(expected);
 
     deepStrictEqual(resultado, expected);
   });
   
   
   
-  // it('Deve cadastrar um heróis usando arquivos', async () => {
-  //   const expected = DEFAULT_ITEM_CADASTRAR;
+  it('Deve cadastrar um heróis usando arquivos', async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR;
 
-  //   ok(null, expected);
-  // })
+    const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+
+    const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
+
+    deepStrictEqual(actual, expected);
+  })
+
+  it('Deve remover o heróis por id', async () => {
+    const expected = true;
+    
+    const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+    
+    deepStrictEqual(resultado, expected);
+  })
+
+  it('Deve atualizar o herói pelo id', async () => {
+    const expected = {
+      ...DEFAULT_ITEM_ATUALIZAR,
+      nome: 'Batman',
+      poder: 'Dinheiro'
+    }
+
+    const novoDado = {
+      nome: 'Batman',
+      poder: 'Dinheiro'
+    }
+
+    await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDado);
+
+    const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id);
+
+    deepStrictEqual(resultado, expected);
+
+  })
+
+  // Remove os itens cadastrados no before
+  after(async () => {
+    await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+    await database.remover(DEFAULT_ITEM_ATUALIZAR.id);
+  })
 })
